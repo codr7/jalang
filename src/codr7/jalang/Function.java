38ec3e9f595c6880e4ff9d5302e8952fae5132ab
Andreas <codr7@protonmail.com>
2023-12-25 01:13:42 +0100

    Convert function to record type.

--- src/codr7/jalang/Function.java
+++ src/codr7/jalang/Function.java
@@ -1,19 +1,9 @@
 package codr7.jalang;
 
-public class Function {
+public record Function(String name, int arity, Body body) {
   public static final Type<Function> type = new Type<>("Function");
   public interface Body {
     void call(final Vm vm, final Location location, final int arity, final int register);
-  }
-
-  public Function(final String name, final int arity, final Body body) {
-    this.name = name;
-    this.arity = arity;
-    this.body = body;
-  }
-
-  public final int arity() {
-    return arity;
   }
 
   public final void call(final Vm vm, final Location location, final int arity, final int register) {
@@ -23,8 +13,4 @@ public class Function {
   public final String toString() {
     return String.format("(Function %s/%d)", name, arity);
   }
-
-  private final String name;
-  private final int arity;
-  private final Body body;
 }
