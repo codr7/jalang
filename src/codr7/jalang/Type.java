2d4edd83ace72a3526be3eb76c5bddf6013d98a5
Andreas <codr7@protonmail.com>
2024-01-04 03:19:53 +0100

    Cleaning.

--- src/codr7/jalang/Type.java
+++ src/codr7/jalang/Type.java
@@ -6,27 +6,21 @@ public class Type<D> {
   public Type(final String name) {
     this.name = name;
   }
-
   public String dump(final D value) {
     return value.toString();
   }
-
   public boolean equalValues(final D left, final D right) {
     return left.equals(right);
   }
-
   public final String name() {
     return name;
   }
-
   public boolean isTrue(final D value) {
     return true;
   }
-
   public String say(final D value) {
     return dump(value);
   }
-
   public final String toString() {
     return name;
   }
